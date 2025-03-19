51963c698afaa969773c33036d679b68258c2a44
Dalla Cindy <dallacindy06gmail.com>
2025-03-19 16:37:14 +0100

    somme de deux nombres

---
+++ module_somme.py
@@ -0,0 +1,2 @@
+def Somme(x, y):
+    return x+y
---
+++ projet.py
@@ -0,0 +1,5 @@
+from module_somme import Somme
+nombre_1 = int(input("entrer un premier nombre : "))
+nombre_2 = int(input("entrer un deuxieme nombre : "))
+result = Somme(x=nombre_1, y=nombre_2)
+print(f"la somme de ces nombres est : {result}")
